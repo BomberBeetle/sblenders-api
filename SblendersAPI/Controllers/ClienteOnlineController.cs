3987a2c8dd84b13443b7ebb98e12464457bd0ced
BomberBeetle <36681076+BomberBeetle@users.noreply.github.com>
2020-04-03 13:01:21 -0300

    Adicionado GET ClienteOnline (usado por  funcionarios)

--- SblendersAPI/Controllers/ClienteOnlineController.cs
+++ SblendersAPI/Controllers/ClienteOnlineController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -13,19 +15,83 @@ namespace SblendersAPI.Controllers
     {
         // GET: api/ClienteOnline
         [HttpGet("{userid}/{clientid}", Name = "GetOnlineClient")]
-        public IEnumerable<string> Get()
+        public Dictionary<string, object> Get(string userid, string clientid)
         {
+            string token = Request.Headers["Authorization"];
 
-            return new string[] {};
-           
+            using (
+              SqlConnection connection = new SqlConnection(string.Format(
+                  "User ID={0}; Password={1}; Initial Catalog={2}; Persist Security Info=True;Data Source={3}"
+                  , Program.dbLogin, Program.dbPass, "dbSblenders", Program.dbEnv))
+              )
+
+            using (
+                SqlCommand userQueryCommand = new SqlCommand(
+                    "SELECT agenteID, tipoAgenteID FROM tbAgente WHERE agenteID= @id AND agenteToken = @token;"
+                    , connection)
+            )
+            {
+
+                userQueryCommand.Parameters.Add(new SqlParameter("@token", token));
+                userQueryCommand.Parameters.Add(new SqlParameter("@id", userid));
+
+                using (SqlDataAdapter userQueryAdapter = new SqlDataAdapter(userQueryCommand))
+                {
+                    DataTable userQuery = new DataTable();
+                    userQueryAdapter.Fill(userQuery);
+
+                    if (userQuery.Rows.Count < 1)
+                    {
+                        Response.StatusCode = 403;
+                        return new Dictionary<string, object>
+                    {
+                        { "error", "AUTH_ERROR" }
+                    };
+                    }
+
+                    if ((int)userQuery.Rows[0]["tipoAgenteID"] == 2) //tem que ser funcionario
+                    {
+                        using (SqlCommand clientQueryCommand = new SqlCommand(
+                            "SELECT clienteOnlineNome, clienteOnlineSobrenome FROM tbClienteOnline WHERE agenteID= @id;",
+                            connection))
+                        {
+                            clientQueryCommand.Parameters.Add(new SqlParameter("@id", clientid));
+
+                            using (SqlDataAdapter clientQueryAdapter = new SqlDataAdapter(clientQueryCommand))
+                            {
+                                DataTable clientTable = new DataTable();
+                                clientQueryAdapter.Fill(clientTable);
+
+                                if (clientTable.Rows.Count < 1)
+                                {
+                                    Response.StatusCode = 404;
+                                    return new Dictionary<string, object>
+                                    {
+                                        { "error", "CLIENT_NOT_FOUND_ERROR" }
+                                    };
+                                }
+
+                                else
+                                {
+                                    return new Dictionary<string, object>
+                                    {
+                                        {"clientName", clientTable.Rows[0]["clienteOnlineNome"].ToString()},
+                                        {"clientSurname", clientTable.Rows[0]["clienteOnlineSobrenome"].ToString()}
+                                    };
+                                }
+
+                            }
+                        }
+                    }
+                    Response.StatusCode = 403;
+                    return new Dictionary<string, object>
+                    {
+                        { "error", "AUTH_ERROR" }
+                    };
+                }  
+            }
         }
 
-
-        // POST: api/ClienteOnline
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
 
         // PUT: api/ClienteOnline/5
         [HttpPut("{id}")]
