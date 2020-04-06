75b97dfd62e704d3879aac7fa965f8ad2325b194
BomberBeetle <36681076+BomberBeetle@users.noreply.github.com>
2020-04-05 23:22:02 -0300

    produtos index

---
+++ SblendersAPI/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SblendersAPI.Models
+{
+    public class Produto
+    {
+        public int ID;
+        public decimal Cost;
+        public string Name;
+        
+        public Produto(int ID, decimal Cost, string Name)
+        {
+            this.ID = ID;
+            this.Cost = Cost;
+            this.Name = Name;
+        }
+    }
+}
