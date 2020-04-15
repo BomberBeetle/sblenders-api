59049c80ba2db5ed0153c441266128ca73f767b4
BomberBeetle <36681076+BomberBeetle@users.noreply.github.com>
2020-04-14 22:47:38 -0300

    get foto ingrediente

---
+++ SblendersAPI/Controllers/IngredienteFotoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SblendersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredienteFotoController : ControllerBase
+    {
+        // GET: api/IngredienteFoto/5
+        [HttpGet("{id}", Name = "Get")]
+        public HttpResponseMessage Get(int id)
+        {
+            using (
+                SqlConnection connection = new SqlConnection(string.Format("User ID={0}; Password={1}; Initial Catalog={2}; Persist Security Info=True;Data Source={3}", Program.dbLogin, Program.dbPass, "dbSblenders", Program.dbEnv))
+            )
+            using (
+               SqlCommand selectIngredientPhotoCommand = new SqlCommand("SELECT ingredienteFoto FROM tbIngrediente WHERE ingredienteID = @id", connection)
+                )
+            {
+                selectIngredientPhotoCommand.Parameters.Add(new SqlParameter("@id", id));
+                byte[] photoFile = (byte[])selectIngredientPhotoCommand.ExecuteScalar();
+                HttpResponseMessage responseMessage = new HttpResponseMessage();
+                responseMessage.Content = new ByteArrayContent(photoFile);
+                responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpg");
+                return responseMessage;
+            }
+        }
+    }
+}
