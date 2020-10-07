73faf4093c9634a5180ab10099a5b7d142098ef3
Guimel <GuimelXD@users.noreply.github.com>
2020-10-07 13:58:47 -0300

    Correção de erros

--- SblendersAPI/Controllers/FuncionarioFotoController.cs
+++ SblendersAPI/Controllers/FuncionarioFotoController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -16,32 +17,31 @@ namespace SblendersAPI.Controllers
     {
         // GET: api/FuncionarioFoto/5
         [HttpGet("{id}", Name = "GetFuncionarioFoto")]
-        public HttpResponseMessage Get(int id)
+        public IActionResult Get(int id)
         {
             using (
                 SqlConnection connection = new SqlConnection(string.Format("User ID={0}; Password={1}; Initial Catalog={2}; Persist Security Info=True;Data Source={3}", Program.dbLogin, Program.dbPass, "dbSblenders", Program.dbEnv))
             )
             using (
-                SqlCommand authEmpCommand = new SqlCommand("SELECT agenteID FROM tbAgente WHERE agentID = @id AND agentToken=@token AND tipoFuncionarioID = 2", connection)
+                SqlCommand authEmpCommand = new SqlCommand("SELECT agenteID FROM tbAgente WHERE agenteID = @id AND agenteToken=@token AND tipoAgenteID = 2", connection)
             )
             {
+                authEmpCommand.Parameters.Add(new SqlParameter("@id", id));
+                authEmpCommand.Parameters.Add(new SqlParameter("@token", Request.Headers["Authorization"].ToString()));
                 connection.Open();
                 int? agentID = (int?)authEmpCommand.ExecuteScalar();
-                if(agentID == null)
+                if (agentID == null)
                 {
                     Response.StatusCode = 403;
-                    return new HttpResponseMessage();
+                    return StatusCode(403);
                 }
                 using (
-                SqlCommand selectEmpPhotoCommand = new SqlCommand("SELECT funcionarioFoto FROM tbFuncionario WHERE agentID = @id", connection)
+                SqlCommand selectEmpPhotoCommand = new SqlCommand("SELECT funcionarioFoto FROM tbFuncionario WHERE agenteID = @id", connection)
                  )
                 {
                     selectEmpPhotoCommand.Parameters.Add(new SqlParameter("@id", id));
                     byte[] photoFile = (byte[])selectEmpPhotoCommand.ExecuteScalar();
-                    HttpResponseMessage responseMessage = new HttpResponseMessage();
-                    responseMessage.Content = new ByteArrayContent(photoFile);
-                    responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpg");
-                    return responseMessage;
+                    return File(photoFile, "image/jpg");
                 }
             }
         }
